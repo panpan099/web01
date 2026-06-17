a138eed5fba88d0443f18ecdf818638e86022148
Yanaphat <kpan2066@gmail.com>
2026-06-17 11:46:02 +0700

    ส่งLab1_4 684245035

--- first.ts
+++ first.ts
@@ -1,6 +1,6 @@
 
 //let course: string = "Object-oriented Programming";
-//let mark: number = 80;
+//let marks: number = 80;
 //let pass: boolean = true;
 //console.log("Pass: ",pass);
 //let courses: string[] = ["database","programming","web"];
@@ -11,12 +11,17 @@
 //console.log(courses[1] , marks[1]);
 //console.log(courses[2] , marks[2]);
 
-let fname: string = "yanaphat";
-let lname: string = "naudom";
-let age: number = 21;
-let dreamjob: string = "programmer";
-let mark: number[] = [70,80,90];
-let total = mark[0] + mark[1] + mark[2];
-console.log("total mark: ",total)
-console.log("Name: %s %s",fname,lname);
-console.log("age: %d\ndreamjob: %s",age,dreamjob);
+//let fname: string = "yanaphat";
+//let lname: string = "naudom";
+//let age: number = 21;
+//let dreamjob: string = "programmer";
+//let mark: number[] = [70,80,90];
+//let total = mark[0] + mark[1] + mark[2];
+//console.log("total mark: ",total)
+//console.log("Name: %s %s",fname,lname);
+//console.log("age: %d\ndreamjob: %s",age,dreamjob);
+let income: number = 50000;
+let tax = income * 0.07;
+let net = income - tax;
+
+console.log("Gross Income: %d\ntax rate: 7%\nTax paid: %d\nNet income after tax : %d",income,tax,net)

5d7ea47b5f0826aa31c4acae3aac189a536f259a
Yanaphat <kpan2066@gmail.com>
2026-06-17 11:22:50 +0700

    first 684245035

---
+++ first.ts
@@ -0,0 +1,22 @@
+
+//let course: string = "Object-oriented Programming";
+//let mark: number = 80;
+//let pass: boolean = true;
+//console.log("Pass: ",pass);
+//let courses: string[] = ["database","programming","web"];
+//let marks: number[] =[75,72,81];
+//console.log(marks);
+//console.log("\n");
+//console.log(courses[0] , marks[0]);
+//console.log(courses[1] , marks[1]);
+//console.log(courses[2] , marks[2]);
+
+let fname: string = "yanaphat";
+let lname: string = "naudom";
+let age: number = 21;
+let dreamjob: string = "programmer";
+let mark: number[] = [70,80,90];
+let total = mark[0] + mark[1] + mark[2];
+console.log("total mark: ",total)
+console.log("Name: %s %s",fname,lname);
+console.log("age: %d\ndreamjob: %s",age,dreamjob);
